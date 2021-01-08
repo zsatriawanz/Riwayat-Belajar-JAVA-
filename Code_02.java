3cfba11f48917ccfc1129fe48ebc408bfaef0cdb
Satriawan <72358659+zsatriawanz@users.noreply.github.com>
2021-01-08 14:12:45 +0700

    sealamat datang

---
+++ Code_02.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Code_02{
+	public static void main(String args[]){
+		JOptionPane.showMessageDialog(null,"Selamat datang !","Sapaan",JOptionPane.INFORMATION_MESSAGE);
+		System.exit(0);
+	}
+}
---
+++ Code_03.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+class Code_03
+{
+	public static void main (String args[])
+	{
+		JOptionPane.showMessageDialog(null,"Selamat\nDatang\nJava","Sapaan",JOptionPane.INFORMATION_MESSAGE);
+		System.exit(0);
+	}
+}

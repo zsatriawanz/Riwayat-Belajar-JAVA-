e9c560be60a75f332061d58df8c6bd907ac80baf
Satriawan <72358659+zsatriawanz@users.noreply.github.com>
2021-01-08 14:25:59 +0700

    format

---
+++ Code_09.java
@@ -0,0 +1,10 @@
+class Code_09
+{
+	public static void main (String args[])
+	{
+		int satu = 77;
+		double dua = 3.14159;
+
+		System.out.printf("Nilai integer = %d dan nilai double = %f",satu,dua);
+	}
+}

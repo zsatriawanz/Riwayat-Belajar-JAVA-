ba5ad8ee3cd578861cd0c80c118e416292fc9fd9
Satriawan <72358659+zsatriawanz@users.noreply.github.com>
2021-01-08 14:16:43 +0700

    percabangan if

---
+++ Code_04.java
@@ -0,0 +1,25 @@
+public class Code_04{
+
+   // Metoda main
+   public static void main(String[] args) {
+
+      // Deklarasi variabel
+      int nilaiInt;
+      double sisiLebar;
+      boolean nilaiBenar;
+      char nilaiKarakter;
+
+      // Memberi nilai ke variabel
+      nilaiInt = 10;
+      sisiLebar = 5.4;
+      nilaiBenar = true;
+      nilaiKarakter = 'B';
+
+      // Menampilkan hasil
+      System.out.println();
+      System.out.println("nilaiInt = " + nilaiInt);
+      System.out.println("sisiLebar = " + sisiLebar);
+      System.out.println("nilaiBenar = " + nilaiBenar);
+      System.out.println("nilaiKarakter = " + nilaiKarakter);
+   }
+}
